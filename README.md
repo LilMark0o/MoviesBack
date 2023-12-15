cf475193b95695e5309daa2a0a98a20f25dbfd01
Marco Alejandro Ramírez <ma.ramirez23@uniandes.edu.co>
2023-12-15 11:55:21 -0500

    All the basics query's

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MoviesBack
- 
+
+This is the api used for the application. It uses Flask as the framework, and the credentials are in the .env file. The url's make the query's to the DB and get the data for the front.
--- app.py
+++ app.py
@@ -27,10 +27,65 @@ def connect():
 mysql = connect()
 
 
-@app.route('/')
-def index():
+@app.route('/movies')
+@app.route('/movies/<int:page>')
+def get_movies(page: int = None):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM genre")
+    if page:
+        offset = (page - 1) * 10  # Ajuste del offset para páginas basadas en 1
+        cur.execute(
+            "SELECT * FROM movies ORDER BY score DESC LIMIT 10 OFFSET %s", [offset])
+    else:
+        cur.execute("SELECT * FROM movies ORDER BY score DESC LIMIT 10")
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
+
+
+@app.route('/actors')
+@app.route('/actors/<int:page>')
+def get_actors(page: int = None):
+    cur = mysql.connection.cursor()
+    if page:
+        offset = (page - 1) * 10
+        cur.execute(
+            "SELECT * FROM actors ORDER BY names DESC LIMIT 10 OFFSET %s", [offset])
+    else:
+        cur.execute("SELECT * FROM actors ORDER BY names DESC LIMIT 10")
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
+
+
+@app.route('/movie/<int:movie_id>')
+def get_movie(movie_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM movies WHERE id = %s", [movie_id])
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
+
+
+@app.route('/actor/<int:actor_id>')
+def get_actor(actor_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM actors WHERE id = %s", [actor_id])
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
+
+
+@app.route('/movies/search/<string:query>')
+@app.route('/movies/search/<string:query>/<int:page>')
+def search_movies(query: str, page: int = None):
+    cur = mysql.connection.cursor()
+    if page:
+        offset = (page - 1) * 10
+        cur.execute(
+            "SELECT * FROM movies WHERE names LIKE %s ORDER BY score DESC LIMIT 10 OFFSET %s", [f"%{query}%", offset])
+    else:
+        cur.execute(
+            "SELECT * FROM movies WHERE names LIKE %s ORDER BY score DESC LIMIT 10", [f"%{query}%"])
     data = cur.fetchall()
     cur.close()
     return jsonify(data)
